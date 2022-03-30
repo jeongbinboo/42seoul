1d5f15be3bd89dd5cea7bf57bc949df848d63339
jeongbinboo <jrkasey3461@naver.com>
2022-03-30 20:01:52 +0900

    add functions

---
+++ LibFt/libft/ft_strmapi.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *tmp;
+    int     len;
+
+    if (!s)
+        return (0);
+    len = ft_strlen((char *)s);
+    tmp = (char *)malloc(sizeof(char) * (len + 1));
+    if (!tmp)
+        return (0);
+    tmp[len] = '\0';
+    --len;
+    while (len >= 0)
+    {
+        tmp[len] = f((unsigned int)len, s[len]);
+        len--;
+    }
+    return (tmp);
+}
