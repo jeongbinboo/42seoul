d2db876c9fb3d5d96e44273f502441b2cf5ff040
jeongbinboo <jrkasey3461@naver.com>
2022-04-04 11:54:35 +0900

    fix ft_strlcat.c

--- LibFt/libft/ft_strlcat.c
+++ LibFt/libft/ft_strlcat.c
@@ -12,27 +12,21 @@
 #include "libft.h"
 size_t	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	dest_len;
-	unsigned int	src_len;
-	unsigned int	ret;
-	unsigned int	i;
+	size_t	dest_len;
+	size_t	src_len;
+	int		i;
 
-	dest_len = 0;
-	src_len = 0;
-	while (dest[dest_len])
-		dest_len++;
-	while (src[src_len])
-		src_len++;
-	if (size < dest_len)
-		return (src_len + size);
-	ret = src_len + dest_len;
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size <= dest_len)
+		return (size + src_len);
 	i = 0;
-	while (size - dest_len - 1)
+	while (src[i] && size - dest_len -1)
 	{
-		dest[dest_len] = src[i];
-		dest_len++;
+		dest[dest_len + i] = src[i];
+		size--;
 		i++;
 	}
-	dest[dest_len] = 0;
-	return (ret);
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
